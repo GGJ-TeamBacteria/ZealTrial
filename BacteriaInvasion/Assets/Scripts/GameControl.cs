32f125c2b5f6d80cad05caadb11085d71f0662bf
zealyim <Zeal@HWP.local>
2018-01-27 16:31:10 -0800

    add good bat and bad bat

--- BacteriaInvasion/Assets/Scripts/GameControl.cs
+++ BacteriaInvasion/Assets/Scripts/GameControl.cs
@@ -11,6 +11,7 @@ public class GameControl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		SpawnBacteriaGood (numOfBacteriaGood);
+		SpawnBacteriaBad (numOfBacteriaGood);
 	}
 	void SpawnBacteriaGood (int maxHazard) {
 		Quaternion spawnRotation = Quaternion.identity;
@@ -25,7 +26,18 @@ public class GameControl : MonoBehaviour {
 
 		}
 	}
-			void SpawnBacteriaBad() {
+	void SpawnBacteriaBad(int maxHazard) {
+		Quaternion spawnRotation = Quaternion.identity;
+
+		for (int i = 0; i < maxHazard; i++) {
+			Instantiate (BacteriaBad, 
+				new Vector3 (Random.Range (-worldSize.x, worldSize.x), 
+					Random.Range (-worldSize.y, worldSize.y),
+					Random.Range (-worldSize.z, worldSize.z)),
+				spawnRotation
+			);
+
+		}
 
 	}
 
