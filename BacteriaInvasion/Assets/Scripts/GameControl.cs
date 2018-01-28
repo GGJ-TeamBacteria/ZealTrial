af93268dfb1d6d766aafbad93966e22d7375cd81
zealyim <Zeal@HWP.local>
2018-01-27 16:10:31 -0800

    First upload

---
+++ BacteriaInvasion/Assets/Scripts/GameControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+	public GameObject BacteriaGood;
+	public GameObject BacteriaBad;
+	public Vector3 worldSize;
+	public int numOfBacteriaGood;
+
+	// Use this for initialization
+	void Start () {
+		SpawnBacteriaGood (numOfBacteriaGood);
+	}
+	void SpawnBacteriaGood (int maxHazard) {
+		Quaternion spawnRotation = Quaternion.identity;
+
+		for (int i = 0; i < maxHazard; i++) {
+			Instantiate (BacteriaGood, 
+				new Vector3 (Random.Range (-worldSize.x, worldSize.x), 
+					Random.Range (-worldSize.y, worldSize.y),
+					Random.Range (-worldSize.z, worldSize.z)),
+				spawnRotation
+			);
+
+		}
+	}
+			void SpawnBacteriaBad() {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
